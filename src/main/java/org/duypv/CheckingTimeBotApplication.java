c998e1a119618580362778d63e48b6d1dc2a9f05
duy.pham3 <duy.pham3@ntq-solution.com.vn>
2025-11-19 22:08:59 +0700

    apply spring boot

---
+++ src/main/java/org/duypv/CheckingTimeBotApplication.java
@@ -0,0 +1,48 @@
+package org.duypv;
+
+import org.duypv.bot.CheckingTimeBot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SpringBootApplication
+public class CheckingTimeBotApplication {
+	private static final Logger log = LoggerFactory.getLogger(CheckingTimeBotApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckingTimeBotApplication.class, args);
+
+		registerBot();
+	}
+
+	private static void registerBot() {
+		boolean running = true;
+
+		while (running) {
+			try {
+				TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+				botsApi.registerBot(new CheckingTimeBot());
+
+				log.info("Bot started successfully!");
+				running = false;
+
+			} catch (TelegramApiException e) {
+				log.error("Can't connect to Telegram: {}", e.getMessage());
+				log.error("Reconnect after 60 seconds...");
+
+				try {
+					Thread.sleep(60_000);
+				} catch (InterruptedException ie) {
+					Thread.currentThread().interrupt();
+					log.info("Main thread interrupted, stopping application.");
+					running = false;
+				}
+			}
+		}
+	}
+
+}
---
+++ src/main/java/org/duypv/controller/HealthController.java
@@ -0,0 +1,18 @@
+package org.duypv.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+  @GetMapping("/")
+  public String home() {
+    return "Bot is running!";
+  }
+
+  @GetMapping("/health")
+  public String health() {
+    return "OK";
+  }
+}
